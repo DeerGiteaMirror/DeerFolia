91045ef5915a236390a0f01ffef033202fc9b7b2
zhangyuheng <zhangyuheng@lunadeer.cn>
2025-03-09 16:36:03 +0800

    Rebranding

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,71 @@
-import io.papermc.paperweight.util.*
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import io.papermc.paperweight.tasks.RebuildGitPatches
 
 plugins {
-    java
-    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
-    id("io.papermc.paperweight.patcher") version "1.7.7"
+    java // TODO java launcher tasks
+    id("io.papermc.paperweight.patcher") version "2.0.0-beta.14"
 }
 
-allprojects {
-    apply(plugin = "java")
+paperweight {
+    upstreams.register("folia") {
+        ref = providers.gradleProperty("foliaRef")
+        repo = github("PaperMC", "Folia")
 
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(21))
+        patchFile {
+            path = "folia-server/build.gradle.kts"
+            outputFile = file("deer-folia-server/build.gradle.kts")
+            patchFile = file("deer-folia-server/build.gradle.kts.patch")
+        }
+        patchFile {
+            path = "folia-api/build.gradle.kts"
+            outputFile = file("deer-folia-api/build.gradle.kts")
+            patchFile = file("deer-folia-api/build.gradle.kts.patch")
+        }
+        patchRepo("paperApi") {
+            upstreamPath = "paper-api"
+            patchesDir = file("deer-folia-api/paper-patches")
+            outputDir = file("paper-api")
+        }
+        patchDir("foliaApi") {
+            upstreamPath = "folia-api"
+            excludes = listOf("build.gradle.kts", "build.gradle.kts.patch", "paper-patches")
+            patchesDir = file("deer-folia-api/folia-patches")
+            outputDir = file("folia-api")
         }
     }
+
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 
-repositories {
-    mavenCentral()
-    mavenLocal()
-    maven(paperMavenPublicUrl) {
-        content {
-            onlyForConfigurations(configurations.paperclip.name)
+subprojects {
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+
+    extensions.configure<JavaPluginExtension> {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
         }
     }
-}
 
-dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.10.3:fat")
-    decompiler("org.vineflower:vineflower:1.10.1")
-    paperclip("io.papermc:paperclip:3.0.3")
-}
+    repositories {
+        mavenCentral()
+        maven(paperMavenPublicUrl)
+    }
 
-subprojects {
-    tasks.withType<JavaCompile>().configureEach {
+    dependencies {
+        "testRuntimeOnly"("org.junit.platform:junit-platform-launcher")
+    }
+
+    tasks.withType<AbstractArchiveTask>().configureEach {
+        isPreserveFileTimestamps = false
+        isReproducibleFileOrder = true
+    }
+    tasks.withType<JavaCompile> {
         options.encoding = Charsets.UTF_8.name()
         options.release = 21
+        options.isFork = true
     }
     tasks.withType<Javadoc> {
         options.encoding = Charsets.UTF_8.name()
@@ -45,71 +73,34 @@ subprojects {
     tasks.withType<ProcessResources> {
         filteringCharset = Charsets.UTF_8.name()
     }
-
-    repositories {
-        mavenCentral()
-        maven(paperMavenPublicUrl)
-        maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-        maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
-        maven("https://jitpack.io")
-        maven("https://repo.codemc.io/repository/maven-public/")
-    }
-}
-
-paperweight {
-    serverProject.set(project(":deer-folia-server"))
-
-    remapRepo.set(paperMavenPublicUrl)
-    decompileRepo.set(paperMavenPublicUrl)
-
-    useStandardUpstream("Folia") {
-        url.set("https://ssl.lunadeer.cn:14446/mirror/Folia.git")
-        ref.set(providers.gradleProperty("foliaRef"))
-
-        withStandardPatcher {
-            baseName("Folia")
-
-            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
-            apiOutputDir.set(layout.projectDirectory.dir("deer-folia-api"))
-
-            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
-            serverOutputDir.set(layout.projectDirectory.dir("deer-folia-server"))
-        }
-        patchTasks.register("generatedApi") {
-            isBareDirectory = true
-            upstreamDirPath = "paper-api-generator/generated"
-            patchDir = layout.projectDirectory.dir("patches/generatedApi")
-            outputDir = layout.projectDirectory.dir("paper-api-generator/generated")
+    tasks.withType<Test> {
+        testLogging {
+            showStackTraces = true
+            exceptionFormat = TestExceptionFormat.FULL
+            events(TestLogEvent.STANDARD_OUT)
         }
     }
-}
 
-tasks.register("updateFoliaRef") {
-    // 更新 Folia 依赖的最新 commit
-    val tempDir = layout.cacheDir("foliaRefLatest")
-    val file = "gradle.properties"
-
-    doFirst {
-        val foliaLatestCommitJson = layout.cache.resolve("foliaLatestCommit.json")
-        download.get().download("https://ssl.lunadeer.cn:14446/api/v1/repos/mirror/Folia/commits?sha=master", foliaLatestCommitJson)
-        val foliaLatestCommit = gson.fromJson<paper.libs.com.google.gson.JsonArray>(foliaLatestCommitJson).get(0).asJsonObject.get("sha").asString
-
-        copy {
-            from(file)
-            into(tempDir)
-            filter { line: String ->
-                line.replace("foliaRef = .*".toRegex(), "foliaRef = $foliaLatestCommit")
+    extensions.configure<PublishingExtension> {
+        repositories {
+            /*
+            maven("https://repo.papermc.io/repository/maven-snapshots/") {
+                name = "paperSnapshots"
+                credentials(PasswordCredentials::class)
             }
+             */
         }
     }
+}
 
+tasks.register("printMinecraftVersion") {
     doLast {
-        copy {
-            from(tempDir.file("gradle.properties"))
-            into(project.file(file).parent)
-        }
+        println(providers.gradleProperty("mcVersion").get().trim())
+    }
+}
+
+tasks.register("printPaperVersion") {
+    doLast {
+        println(project.version)
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,16 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
+        mavenLocal()
         maven("https://papermc.io/repo/repository/maven-public/")
     }
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
 rootProject.name = "DeerFolia"
 
-include("deer-folia-api", "deer-folia-server")
+include("deer-folia-api")
+include("deer-folia-server")
